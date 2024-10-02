6ef21aa4aa243889fdb1266509f45079578ac9e0
Isaac <asabaisaac1@gmail.com>
2024-10-02 18:49:51 +0200

    item_id

--- fastapi-tutorial/main.py
+++ fastapi-tutorial/main.py
@@ -12,6 +12,14 @@ async def base_route():
 async def post():
     return {"message": "This is a new post in the file"}
 
+@app.get("/items")
+async def list_items():
+    return {"message": "This is a list of items"}
+
+@app.get("/items/{item_id}")
+async def get_items(item_id:int):
+    return {"item_id":item_id}
+
 @app.put("/")
 async def put():
     return {"message":"This is my out test"}
